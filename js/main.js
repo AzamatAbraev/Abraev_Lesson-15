c58618d4e8dc15cf627a7a51a4415117def92a9a
AzamatAbraev <azamatabraev07@gmail.com>
2023-07-01 00:51:28 +0500

    Lesson 15

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// aside
+
+const sidebar = document.querySelector("aside");
+const sidebarToggle = document.querySelector(".hamburger");
+
+sidebarToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+
+if (innerWidth <= 1000) {
+  sidebar.classList.add("close");
+} else {
+  sidebar.classList.remove("close");
+}
+
+// dark-light
+
+let modeBtn = document.getElementById("dark-light");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "light") {
+    this.firstElementChild.src = "images/aside/dark.svg";
+  } else {
+    this.firstElementChild.src = "images/aside/light.svg";
+  }
+  document.body.classList.toggle("light");
+});
